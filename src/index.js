14748530221d5fc3df08520eded37f6ac237bd73
doantuan22 <tuan0947881956@gmail.com>
2026-04-03 23:17:23 +0700

    123

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars/dist/lib')
+const path = require('path')
+const app  =  express()
+const port  = 3000
+
+app.use(morgan('combined'))
+
+app.engine('hbs', engine({
+    extname : '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources\\views'))
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/tin_new', (req, res) => {
+    res.render('new')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
